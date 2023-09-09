7baac09fa702ce32dd15ba113eafa5227a2890ac
Genji-kun <2051052097phat@ou.edu.vn>
2023-09-09 13:37:05 +0700

    udpate

--- oupia/src/pages/Home/index.jsx
+++ oupia/src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './style.scss';
+import { UserContext } from '../../App';
 
 const Home = () => {
-    const [isScrolled, setIsScrolled] = useState(false);
+    const [, setIsScrolled] = useState(false);
+    const [currentUser,] = useContext(UserContext);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -22,23 +25,39 @@ const Home = () => {
 
     return (
         <>
-            <section className="bg-center h-screen bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/dLinkocs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply pt-10">
+            <div className="object-cover h-screen bg-cover bg-home bg-gray-500 bg-blend-multiply pt-10">
                 <div className="px-4 mx-auto text-center h-full w-full flex flex-col justify-center">
-                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
-                    <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-                    <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                        <Link to="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                            Get started
-                            <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
-                        </Link>
-                        <Link to="#" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
-                            Learn more
-                        </Link>
-                    </div>
+                    {!currentUser ? <>
+                        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Nhà trọ sinh viên giá rẻ</h1>
+                        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Truy cập Oupia ngay để biết thêm về thông tin nhà trọ gần trường đại học của bạn, hãy đăng ký ngay</p>
+                        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                            <Link to="/register" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blueTemplate hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                                Đăng ký ngay
+                                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </Link>
+                            <Link to="/posts" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                                Xem bài đăng nhà trọ
+                            </Link>
+                        </div>
+                    </> : <>
+                        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Xin chào, <span className="text-blueTemplate">{currentUser.fullName}</span></h1>
+                        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48"></p>
+                        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                            <Link to="/upload" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blueTemplate hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                                Đăng bài viết mới
+                                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </Link>
+                            <Link to="/posts" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                                Xem bài đăng nhà trọ
+                            </Link>
+                        </div>
+                    </>}
                 </div>
-            </section>
+            </div>
         </>
     );
 };
