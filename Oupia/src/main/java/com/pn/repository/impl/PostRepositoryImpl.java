3b8d52a508104dd5cbb75f71d5996556ac9675f8
NganBao0206 <yuu.mmd02@gmail.com>
2023-09-10 22:51:37 +0700

    fix back

--- Oupia/src/main/java/com/pn/repository/impl/PostRepositoryImpl.java
+++ Oupia/src/main/java/com/pn/repository/impl/PostRepositoryImpl.java
@@ -87,7 +87,6 @@ public class PostRepositoryImpl implements PostRepository {
             if (type != null && !type.isEmpty()) {
                 if (type.equals("tenantPost")) {
                     predicates.add(b.isNotNull(root.get("postFindDetail").get("id")));
-                    q.multiselect(root, join.get("imageSet"));
                 } else if (type.equals("landlordPost")) {
                     predicates.add(b.isNotNull(root.get("postRentDetail").get("id")));
                 }
